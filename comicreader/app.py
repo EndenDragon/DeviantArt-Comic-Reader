21ffbb4a2c96c8ab7a3565bc6b7cff6b3d4acda0
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-07-03 00:19:59 -0700

    Implement flask-sqlalchemy

--- comicreader/app.py
+++ comicreader/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, url_for
 from config import config
-import os
 import blueprints.fetch
 import blueprints.user
+from database import db
+import os
 
 os.chdir(config['APP_LOCATION'])
 app = Flask(__name__, static_folder="../static")
 app.config['SQLALCHEMY_DATABASE_URI'] = config['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = config['SECRET_KEY']
+
+db.init_app(app)
 
 app.register_blueprint(blueprints.fetch.fetch, url_prefix="/fetch")
 app.register_blueprint(blueprints.user.user, url_prefix="/user")
