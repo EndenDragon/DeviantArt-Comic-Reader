76a74f94bb24ce22049f298136f32371e4ac1094
Jeremy Zhang <jeremyzhang5000@gmail.com>
2016-07-16 14:41:49 -0700

    Remove webdesign folder

--- webdesign/run.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("initial.html")
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000,debug=True)
